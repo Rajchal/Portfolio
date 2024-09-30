7d6d3d5d05c4bbbf54a610a6ae5762f84e2c6c02
Rajchal <rajchalanjal1@gmail.com>
2024-09-30 19:49:15 +0545

    lol

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-const app=express();
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 const PORT = 2020;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
