b9e126b06a781cc0b6e6bd0b93ce36f1d247256a
Rajchal <rajchalanjal1@gmail.com>
2024-09-30 14:08:42 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+const PORT = 2020;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
