1ffdaf86e33d71ab674f906928e6d47cd4bc9ece
Rajchal <rajchalanjal1@gmail.com>
2024-11-10 01:08:30 +0545

    First commit and also added responsive navbar to my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from './sections/Navbar'
+const App = () => {
+  return (
+    <>
+      <main className='max-w-7xl mx-0'>
+    <Navbar/>
+    </main>
+    </>
+    
+  )
+}
+
+export default App
