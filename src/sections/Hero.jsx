40c9fa269f2dc065fadb4c5c811fd37af9447762
Rajchal <rajchalanjal1@gmail.com>
2024-11-11 11:19:38 +0545

    made the 3d model responsive

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,17 +3,25 @@ import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
 import HackerRoom from "/src/components/HackerRoom";
 import CanvasLoader from "../components/CanvasLoader";
-import { Leva, useControls } from "leva";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants";
 const Hero = () => {
-  const x = useControls("HackerRoom", {
-    positionX: { value: 2.5, min: -10, max: 10 },
-    positionY: { value: 2.5, min: -20, max: 10 },
-    positionZ: { value: 2.5, min: -10, max: 10 },
-    rotationX: { value: 2.5, min: -10, max: 10 },
-    rotationY: { value: 2.5, min: -10, max: 10 },
-    rotationZ: { value: 2.5, min: -10, max: 10 },
-    scale: { value: 0.07, min: 0.01, max: 0.1 },
-  });
+  // const x = useControls("HackerRoom", {
+  //   positionX: { value: 2.5, min: -10, max: 10 },
+  //   positionY: { value: 2.5, min: -20, max: 10 },
+  //   positionZ: { value: 2.5, min: -10, max: 10 },
+  //   rotationX: { value: 2.5, min: -10, max: 10 },
+  //   rotationY: { value: 2.5, min: -10, max: 10 },
+  //   rotationZ: { value: 2.5, min: -10, max: 10 },
+  //   scale: { value: 0.07, min: 0.01, max: 0.1 },
+  // });
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ minWidth: 440, maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen border-2 border-blue-500 w-full flex flex-xol relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -25,14 +33,14 @@ const Hero = () => {
         </p>
       </div>
       <div className="w-full h-full absolute inset-0">
-        <Leva />
+        {/* <Leva /> */}
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
             <HackerRoom
-              position={[-0.5, -15.4, -8.5]}
+              position={sizes.deskPosition}
               rotation={[3.3, 2.7, 3.15]}
-              scale={0.1}
+              scale={sizes.deskScale}
             />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
