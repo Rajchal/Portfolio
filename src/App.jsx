d3353acf974047a5611831f0edf648f5461d6453
Rajchal <rajchalanjal1@gmail.com>
2024-11-14 16:45:21 +0545

    Added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import About from "./sections/About";
+
 const App = () => {
   return (
     <>
-      <main className="max-w-screen mx-0 bg-slate-900">
+      <main className="max-w-screen mx-0">
         <Navbar />
         <Hero />
+        <About />
       </main>
     </>
   );
---
+++ src/sections/About.jsx
@@ -0,0 +1,126 @@
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
+import { useState } from "react";
+
+const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("rajchalanjal1@gmail.com");
+    setHasCopied(true);
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+  return (
+    <section className="c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">Hi, I&apos;m Anjal Rajchal</p>
+              <p className="grid-subtext">
+                With 2 years of experience, I have honed my skills in frontend
+                and backend devlopment, with a focus on animated 3D websites.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/greed.png"
+              alt="grid-2"
+              className="w-full sm:w-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">Technologies I use</p>
+              <p className="grid-subtext">
+                I specialize in JavaScript/TypeScript with a focus on React and
+                Next.js ecosystem
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[
+                  {
+                    lat: 27.6973,
+                    lng: 85.3318,
+                    text: "I'm Here!",
+                    color: "red",
+                    size: 100,
+                  },
+                ]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I work remotely across most timezones
+              </p>
+              <p className="grid-subtext">
+                I&apos;m based in Kathmandu, Nepal, and work with clients
+                worldwide
+              </p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">My passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code. Coding
+                isn&apos;t just my profession -it is my passion.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="alt-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center ">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  rajchalanjal1@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
